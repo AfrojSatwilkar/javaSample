d8018bdf7d0f0963b82d2aee38bf1aac24e4019e
Afroj Satwilkar <88261856+AfrojSatwilkar@users.noreply.github.com>
2021-09-28 16:05:34 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,17 @@
 package com.company;
 
+class Name {
+    public void firstName() {
+        int a=5;
+        int b=7;
+        int sum =a+b;
+        System.out.println(sum);
+    }
+}
 public class HelloWorld {
     public static void main(String[] args) { 
         System.out.println("Hello World !'");
+        Name s=new Name;
+        s.firstName();
     }
 }
